bdd35d2d7a7f9c119adeadde59caea4625e61434
Siddhcreator-1706 <shahsiddhb@gmail.com>
2026-01-25 04:48:37 +0530

    refactor: timing issue resolved

--- components/GlobalNavbar.tsx
+++ components/GlobalNavbar.tsx
@@ -4,8 +4,13 @@ import { Navbar } from "@/components/ui/Resizable-navbar";
 import NavigationPanel from "@/components/ui/NavigationPanel";
 import { useNavigationState } from "@/lib/useNavigationState";
 
+import { usePathname } from "next/navigation";
+
 export default function GlobalNavbar() {
     const { isNavbarVisible } = useNavigationState();
+    const pathname = usePathname();
+
+    if (pathname?.startsWith("/admin")) return null;
 
     return (
         <Navbar visible={isNavbarVisible}>
