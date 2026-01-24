46d94f8fbd288a8408b067bd803f295e80c4d48a
Siddhcreator-1706 <shahsiddhb@gmail.com>
2026-01-24 16:17:47 +0530

    refactor: Hall of fame updated

--- components/Home-HallOfFame.tsx
+++ components/Home-HallOfFame.tsx
@@ -31,7 +31,7 @@ const HeroCard = ({
     }}
   >
     <Image
-      src="/images_home/HallOfFame.png"
+      src="/images_home/HallOfFame.jpeg"
       alt="Hall of Fame"
       fill
       className="object-cover"
